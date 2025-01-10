3b2b8b81f7571bd383230b36a01ba2909a64624f
matt <63452179+mathieufitzgerald@users.noreply.github.com>
2025-01-10 20:56:36 +0100

    Add files via upload

---
+++ LibreFollowTests/LibreFollowTests.swift
@@ -0,0 +1,17 @@
+//
+//  LibreFollowTests.swift
+//  LibreFollowTests
+//
+//  Created by Mathieu Fitzgerald on 05.01.2025.
+//
+
+import Testing
+@testable import LibreFollow
+
+struct LibreFollowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
